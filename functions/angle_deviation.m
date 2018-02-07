e3e6c487bfa755fad431361b89f3dbec67104b65
Veljko <vboljanovic@ucla.edu>
2018-02-06 19:11:27 -0800

    New function. Added to examine RMSE vs AOA spread.

--- functions/angle_deviation.m
+++ functions/angle_deviation.m
@@ -2,7 +2,7 @@
 clear;clc;close all
 % rng(2)
 %% other parameter
-noise_pow = 1;
+print_stat = 0;
 
 %% Goemetry Parameters
 Nt = 16;
@@ -11,57 +11,24 @@ D_bs2ue = 40;
 loc0_ue = [-D_bs2ue/2,0];
 loc0_bs = [D_bs2ue/2,0];
 speed_c = 3e8;
-fc = 28e9;
 %% Large Scale Parameter (Multiple Clusters)
-cluster_delay = [300e-9,250e-9];
 cluster_num = 1;
 ray_num = 2;
-angle_dev = 2:18;
+sigma_AOA_spread = 2:2:18; %different deviations for AOA
+SNR = 20; %in dB
+mc = 1000;
 
-
-for angle_index = 1:length(angle_dev)
-    %% Cluster Generation (Strongest Two?)
-    for cluster_index = 1:cluster_num
-
-        delay = cluster_delay(cluster_index);
-        ellipse_len = delay*speed_c;
-        a = ellipse_len/2;
-        c = D_bs2ue/2;
-        b = sqrt(a^2-c^2);
-
-    %     AOA = rand*pi-pi/2;
-        if cluster_index==1
-            AOA = pi/2;
-        else
-            AOA = -pi/2;
-        end
-        loc_cluster_cnt = [a*cos(AOA),b*sin(AOA)];
-
-        for ray_index=1:ray_num
-            loc_cluster = repmat(loc_cluster_cnt,ray_num,1)+[-angle_dev(angle_index),angle_dev(angle_index)];
-        end
-        loc_cluster_total((cluster_index-1)*ray_num+1:cluster_index*ray_num,:) =  loc_cluster;
-
-        for ray_index = 1:ray_num
-            raydelay(cluster_index,ray_index) = (norm(loc_cluster(ray_index,:)-loc0_bs)...
-                        +norm(loc_cluster(ray_index,:)-loc0_ue))/speed_c;
-            temp = loc_cluster(ray_index,:) - loc0_ue;
-            rayAOA(cluster_index,ray_index) = angle(temp(1)+1j*temp(2));
-            temp = loc_cluster(ray_index,:) - loc0_bs;
-            rayAOD(cluster_index,ray_index) = angle(-temp(1)-1j*temp(2));
-        end
-        raygain = exp(1j*rand(cluster_num, ray_num)*2*pi);
-    %     raygain(2,:) = zeros(1,ray_num);
-
-    %     fprintf('Cluster %d:\n',cluster_index);
-    %     fprintf('DS Mean:    %4.2f ns\n', mean(raydelay(cluster_index,:)*1e9));
-    %     fprintf('DS Std Dev: %4.2f ns\n', sqrt(var(raydelay(cluster_index,:)*1e9)));
-    %     fprintf('AOAS Mean:    %4.2f degree\n', mean(rayAOA(cluster_index,:)/pi*180));
-    %     fprintf('AOAS Std Dev: %4.2f degree\n', sqrt(var(rayAOA(cluster_index,:)/pi*180)));
-    %     fprintf('AODS Mean:    %4.2f degree\n', mean(rayAOD(cluster_index,:)/pi*180));
-    %     fprintf('AODS Std Dev: %4.2f degree\n', sqrt(var(rayAOD(cluster_index,:)/pi*180)));
-
-    end
+errorSD_noisy = zeros(length(sigma_AOA_spread),1); %RMSE
+for spread_index = 1:length(sigma_AOA_spread)
+    %% Channel parameters
+    [raygain, raydelay, rayAOA, rayAOD, true_rayAOA, true_rayAOD] = get_chan_parameter_nogeo( print_stat,...
+                                                                                              cluster_num,...
+                                                                                              ray_num,...
+                                                                                              0,...
+                                                                                              0,...
+                                                                                              sigma_AOA_spread(spread_index),...
+                                                                                              0,...
+                                                                                              0 );
 
     %% Frequency Domain MIMO Channel & Frequency Domain Equivalent Beamspace Channel
     Nfft = 512;
@@ -78,56 +45,29 @@ for angle_index = 1:length(angle_dev)
 
     %% ML estimation of angle
     %AOA = rand*pi-pi/2;
-    SNR = -15:5:30;
-    errorSD_noisy = zeros(length(SNR),1); %RMSE
     for noise_index = 1:length(SNR);
-        angle_est = zeros(1000, 1);
-        for i = 1:1000 %1000 Monte Carlo simulations
+        angle_est = zeros(mc, 1);
+        for i = 1:mc %1000 Monte Carlo simulations
             y = 0; %received signal
-            true_rayAOA = zeros(1, cluster_num);
-            true_rayAOD = zeros(1, cluster_num);
             for cluster_index = 1:cluster_num
-                delay = cluster_delay(cluster_index);
-                ellipse_len = delay*speed_c;
-                a = ellipse_len/2;
-                c = D_bs2ue/2;
-                b = sqrt(a^2-c^2);
-                if cluster_index==1
-                    AOA = pi/2;
-                else
-                    AOA = -pi/2;
-                end
-                loc_cluster_cnt = [a*cos(AOA),b*sin(AOA)];
-                temp = loc_cluster_cnt - loc0_ue;
-                true_rayAOA(1,cluster_index) = angle(temp(1)+1j*temp(2));
-                temp = loc_cluster_cnt - loc0_bs;
-                true_rayAOD(1,cluster_index) = angle(-temp(1)-1j*temp(2));
                 atx = exp(1j*(0:Nt-1)'*pi*sin(true_rayAOD(1,cluster_index)))/sqrt(Nt); %we assume transmitter is perfectly aligned
-                arx = exp(1j*(0:Nr-1)'*pi*sin(true_rayAOA(1,cluster_index)))/sqrt(Nr); %we assume transmitter is perfectly aligned
-                arx1 = exp(1j*(0:Nr-1)'*pi*sin(true_rayAOA(1,cluster_index))+2/180*pi)/sqrt(Nr);
-                arx2 = exp(1j*(0:Nr-1)'*pi*sin(true_rayAOA(1,cluster_index))-2/180*pi)/sqrt(Nr);
-                arx3 = exp(1j*(0:Nr-1)'*pi*sin(true_rayAOA(1,cluster_index))-3/180*pi)/sqrt(Nr);
-                arx4 = exp(1j*(0:Nr-1)'*pi*sin(true_rayAOA(1,cluster_index))+1/180*pi)/sqrt(Nr);
-                %if we have multiple clusters, must save information about true
-                %rayAOA, true rayAOD and steering vectors (e.g. make new vectors
-                %and save them into these vectors)
-                F = repmat(atx,1,40);%[atx atx atx atx]; %we assume that receiver makes 4 measurements (for example)
-                W = (randi(2,Nr,40)*2-3)+1j*(randi(2,Nr,40)*2-3);
-                y = y + W'*H_freq0(:,:,1)*F*ones(40,1); %we take just one subcarrier
+                %arx = exp(1j*(0:Nr-1)'*pi*sin(true_rayAOA(1,cluster_index)))/sqrt(Nr); %we assume transmitter is perfectly aligned
+                F = repmat(atx,1,8);%[atx atx atx atx]; %we assume that receiver makes 4 measurements (for example)
+                W = (randi(2,Nr,8)*2-3)+1j*(randi(2,Nr,8)*2-3);
+                y = y + W'*H_freq0(:,:,1)*F*ones(8,1); %we take just one subcarrier
                 signal_power = norm(y)^2;
                 noise_power = signal_power / 10^(SNR(noise_index)/10);
-                noise_vec = (randn(40,1)*sqrt(noise_power/80) + 1i*randn(40,1)*sqrt(noise_power/80));
+                noise_vec = (randn(8,1)*sqrt(noise_power/16) + 1i*randn(8,1)*sqrt(noise_power/16));
                 y = y + noise_vec;
             end
             angle_est(i,1) = ml_angle(y, true_rayAOA, true_rayAOD, F, W, cluster_num, Nt, Nr);
         end
-        errorSD_noisy(noise_index,1) = sqrt(mean((angle_est - ones(1000,1)*(true_rayAOA(1,1)/pi*180)).^2));
+        errorSD_noisy(spread_index,1) = sqrt(mean((angle_est - ones(mc,1)*(true_rayAOA(1,1)/pi*180)).^2));
         %angle_error_var = var(angle_est);
     end
 end
-close all
 figure
-semilogy(SNR, errorSD_noisy, 'o-r', 'Linewidth', 2)
-xlabel('SNR [dB]')
+semilogy(sigma_AOA_spread, errorSD_noisy, '-', 'Linewidth', 1.5)
+xlabel('AOA spread')
 ylabel('RMSE of AOA estimation')
-title('RMSE vs. SNR[dB]')
+title('RMSE vs. AOA spread')
